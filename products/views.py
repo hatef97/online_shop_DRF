5b81b48a6064d6bb92bb0d504c6b84b14ca13a25
Hatef Barin <amir.barin76@gmail.com>
2024-01-16 15:12:49 +0330

    Update view

--- products/views.py
+++ products/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
-from django.db.models import Q, F
+from django.db.models import Q, F, Count, Avg
 
 from .models import Product, Order, OrderItem
 
 
 
 def show_data(requests):
-    queryset = Order.objects.select_related('customer').prefetch_related('items__product').all()
+    queryset = Product.objects.filter(datetime_created__year=2022).aggregate(
+        Count('id')
+    )
+    
+    print(queryset)
 
         
     return render(requests, 'hello.html', {'orders': list(queryset)})
