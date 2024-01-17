e5920527853e4956a390dea0285e334718463b72
Hatef Barin <amir.barin76@gmail.com>
2024-01-17 23:40:34 +0330

    Update view

--- products/views.py
+++ products/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
-from django.db.models import Q, F, Count, Avg
+from django.db.models import Q, F, Count, Avg, Value
 
 from .models import Product, Order, OrderItem
 
 
 
 def show_data(requests):
-    queryset = Product.objects.filter(datetime_created__year=2022).aggregate(
-        Count('id')
-    )
+    queryset = OrderItem.objects.annotate(
+        total=F('quantity') * F('price')
+        ).all()
     
     print(queryset)
 
